76312d5594a6c6466215551a182a1bb2eb043e90
skt-t <co.leveke@duocuc.cl>
2025-05-22 19:35:59 -0400

    Agregue la resta

--- Calculadora.py
+++ Calculadora.py
@@ -5,13 +5,16 @@ valor = 0
 while True:
     print("""seleccione opcion
             1- Sumar 
-
+            2- Resta
         """)
 
     valor = int(input("Elige una opcion: ") )     
 
     if valor == 1:
-        print("la suma es",num1+num2)
+        print("La suma es: ", num1+num2)
+        break;
+    if valor == 2:
+        print("La resta es: ", num1 - num2)
         break;
 
     else:

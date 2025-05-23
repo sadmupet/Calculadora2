3f1d156b6503723ce5b497caffd025f1e036aa8e
sadmupet <cr.reyese@duocuc.cl>
2025-05-22 21:02:26 -0400

    Multiplicacion y division

--- Calculadora.py
+++ Calculadora.py
@@ -5,18 +5,25 @@ valor = 0
 while True:
     print("""seleccione opcion
             1- Sumar 
-            2- Resta
+            2- Restar
+            3- Multiplicar
+            4- dividir 
         """)
 
     valor = int(input("Elige una opcion: ") )     
 
     if valor == 1:
-        print("La suma es: ", num1+num2)
+        print("la suma es",num1+num2)
         break;
     if valor == 2:
-        print("La resta es: ", num1 - num2)
+        print("la resta es",num1-num2)
         break;
-
+    if valor == 3:
+        print("la multiplicacion es",num1*num2)
+        break;
+    if valor == 4:
+        print("la division es",num1/num2)
+        break;
     else:
         print("Opcion incorrecta")
         break;
